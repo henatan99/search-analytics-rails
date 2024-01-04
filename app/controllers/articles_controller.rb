7da677de506fa1839e9f4db495c2ec8e08c86c44
Henok Mekonnen <henatan99@yahoo.com>
2024-01-04 04:34:44 -0500

    Views added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,21 @@
 class ArticlesController < ApplicationController
-    before_action :authenticate_user!, expect: [:index, :show]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :authorize_user!, only: [:edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def new 
-        @article = Article.new
+        @article = current_user.articles.build
     end
 
     def create
-        @article = Article.new(article_params)
+        @article = current_user.articles.build(article_params)
 
         if @article.save
-            redirect_to @article
+            redirect_to @article, notice: 'Article was successfully created'
         else
             render :new, status: :unprocessable_entity 
         end
@@ -42,5 +45,13 @@ class ArticlesController < ApplicationController
     private
         def article_params
             params.require(:article).permit(:title, :body)
+        end
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
+        def authorize_user!
+            redirect_to articles_path, alert: "You are not the author of this article" unless  current_user == @article.user
         end
 end
