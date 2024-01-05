1264b8c736eb14328b58ba865519443c2a443308
Henok Mekonnen <henatan99@yahoo.com>
2024-01-05 08:51:38 -0500

    Add logger

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -20,12 +20,17 @@ class IpAddressesController < ApplicationController
     end
 
     def create
-        @ip_address = IpAddress.new(request.remote_ip)
-             
-        if @ip_address.save
-            render json: { message: 'Search was successfully created.' }, status: :created
-        else
-            render json: { errors: @ip_address.errors.full_messages, remote_ip: request.remote_ip }, status: :unprocessable_entity
+        begin
+            @ip_address = IpAddress.new(request.remote_ip)
+                
+            if @ip_address.save
+                render json: { message: 'Search was successfully created.' }, status: :created
+            else
+                render json: { errors: @ip_address.errors.full_messages, remote_ip: request.remote_ip }, status: :unprocessable_entity
+            end
+        rescue ActiveRecord::RecordNotFound => e
+            Rails.logger.error("User not found: #{e.message}")
+            render json: { error: 'User not found' }, status: :not_found
         end
     end
 
