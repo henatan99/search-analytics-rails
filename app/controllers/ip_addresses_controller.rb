dc659888e98bf507cda816893aba7d11e603d395
Henok Mekonnen <henatan99@yahoo.com>
2024-01-05 09:02:06 -0500

    Fix bug

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -13,24 +13,19 @@ class IpAddressesController < ApplicationController
     end
 
     def show
-        @ip_address = Article.find(params[:id])
+        @ip_address = IpAddress.find(params[:id])
         respond_to do |format|
             format.json { render json: @searches }
         end
     end
 
     def create
-        begin
-            @ip_address = IpAddress.new(request.remote_ip)
+        @ip_address = IpAddress.new(request.remote_ip)
                 
-            if @ip_address.save
-                render json: { message: 'Search was successfully created.' }, status: :created
-            else
-                render json: { errors: @ip_address.errors.full_messages, remote_ip: request.remote_ip }, status: :unprocessable_entity
-            end
-        rescue ActiveRecord::RecordNotFound => e
-            Rails.logger.error("User not found: #{e.message}")
-            render json: { error: 'User not found' }, status: :not_found
+        if @ip_address.save
+            render json: { message: 'Search was successfully created.' }, status: :created
+        else
+            render json: { errors: @ip_address.errors.full_messages, remote_ip: request.remote_ip }, status: :unprocessable_entity
         end
     end
 
